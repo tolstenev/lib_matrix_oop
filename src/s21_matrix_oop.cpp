752c2772b29f540103569cce6b90f4034eb08e0e
Gleb Tolstenev <tolstenev.gleb@mail.ru>
2023-03-28 15:11:43 +0300

    Fix copy constructor, add the license in every file. Rename functions and some variables. Add FillWithOne() function.

--- src/s21_matrix_oop.cpp
+++ src/s21_matrix_oop.cpp
@@ -1,8 +1,20 @@
 /*
- * Copyright (с) 2023 Student of School 21:
- * Gleb Tolstenev
- *
+ * Copyright 2023 Gleb Tolstenev
  * yonnarge@student.21-school.ru
+ *
+ * s21_matrix_oop.cpp is the source code file for s21_matrix_oop library
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 #include "s21_matrix_oop.h"
@@ -18,57 +30,65 @@ S21Matrix::S21Matrix() { S21Matrix(3, 3); }
  * @param cols - number of colomns
  */
 S21Matrix::S21Matrix(int rows, int cols) : rows_(rows), cols_(cols) {
-	rows_ = rows;
-	cols_ = cols;
-	p_ = new double*[rows_]();
-
+	elements_ = new double *[rows_]();
 	for (int i = 0; i < rows_; ++i)
-		p_[i] = new double[cols_]();
+		elements_[i] = new double[cols_]();
 }
 
 /**
  * @brief Copy constructor
- * @param m - the matrix to be copied
+ * @param other - the matrix to be copied
  */
-S21Matrix::S21Matrix(const S21Matrix& m) : S21Matrix(m.rows_, m.cols_) {
-	p_ = new double*[rows_]();
+S21Matrix::S21Matrix(const S21Matrix &other) {
+	rows_ = other.rows_;
+	cols_ = other.cols_;
+	elements_ = new double *[rows_]();
 
 	for (int i = 0; i < rows_; ++i)
-		p_[i] = new double[cols_]();
+		elements_[i] = new double[cols_]();
 
 	for (int i = 0; i < rows_; ++i)
 		for (int j = 0; j < cols_; ++j)
-	    p_[i] = m.p_[i];
+	    elements_[i][j] = other.elements_[i][j];
 }
 
 /**
  * @brief Destructor
  */
 S21Matrix::~S21Matrix() {
-  if (p_) {
+  if (elements_) {
 		for (int i = 0; i < rows_; ++i)
-			delete [] p_[i];
-    delete [] p_;
+			delete [] elements_[i];
+    delete elements_;
   }
 }
 
 /**
  * @brief Fills the matrix with numbers in order from 1 to rows * cols)
  */
-void S21Matrix::fill_by_order() {
+void S21Matrix::FillByOrder() {
   int k = 0;
 	for (int i = 0; i < rows_; ++i)
 		for (int j = 0; j < cols_; ++j)
-			p_[i][j] = ++k;
+			elements_[i][j] = ++k;
+}
+
+/**
+ * @brief Fills the matrix with numbers by 1
+ */
+void S21Matrix::FillWithOne() {
+	for (int i = 0; i < rows_; ++i)
+		for (int j = 0; j < cols_; ++j)
+			elements_[i][j] = 1;
 }
 
 /**
  * @brief Print matrix
  */
-void S21Matrix::print_matrix() {
+void S21Matrix::Print() {
 	for (int i = 0; i < rows_; ++i) {
 		for (int j = 0; j < cols_; ++j)
-			std::cout << p_[i][j] << '\t';
+			std::cout << elements_[i][j] << '\t';
 		std::cout << std::endl;
 	}
 }
@@ -77,14 +97,23 @@ void S21Matrix::print_matrix() {
  * @brief Check the current work
  */
 int main() {
-  int x, y = 1;
 
-  while (x != 0 || y != 0) {
-    std::cin >> x;
-    std::cin >> y;
-    S21Matrix m1(x, y);
-    m1.fill_by_order();
-    m1.print_matrix();
-	}
-  return 0;
+	S21Matrix m1(3, 3);
+	m1.FillByOrder();
+	std::cout << "m1" << std::endl;
+	m1.Print();
+	std::cout << std::endl;
+
+	S21Matrix m2(m1);
+	std::cout << "m2" << std::endl;
+
+	m2.Print();
+	std::cout << std::endl;
+
+	std::cout << "m2 after change" << std::endl;
+	m2.FillWithOne();
+	m2.Print();
+	std::cout << std::endl;
+
+	return 0;
 }
--- src/s21_matrix_oop.h
+++ src/s21_matrix_oop.h
@@ -1,8 +1,20 @@
 /*
- * Copyright (с) 2023 Student of School 21:
- * Gleb Tolstenev
- *
+ * Copyright 2023 Gleb Tolstenev
  * yonnarge@student.21-school.ru
+ *
+ * s21_matrix_oop.h is the header file for s21_matrix_oop library
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 #ifndef __S21MATRIX_H__
@@ -13,7 +25,7 @@
 class S21Matrix {
  private:
   int rows_, cols_;
-  double** p_;
+  double** elements_;
 
  public:
   S21Matrix();
@@ -21,8 +33,9 @@ class S21Matrix {
   S21Matrix(const S21Matrix& m);
   ~S21Matrix();
 
-  void fill_by_order();
-  void print_matrix();
+  void FillByOrder();
+  void FillWithOne();
+  void Print();
 };
 
 #endif
